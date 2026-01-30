20182b2a8d71c6d574156e0830d36bdaef469e1c
SunnyDaze <alekrliddiard@reagan.com>
2026-01-29 19:02:26 -0700

    Re-Added example blinky code from the lab handout as it actually compiled properly this time.

--- lab1/Src/lab1.c
+++ lab1/Src/lab1.c
@@ -1,30 +1,35 @@
 #include "main.h"
 #include "stm32f0xx_hal.h"
 
+#include <stm32f0xx_hal.h>
+
 void SystemClock_Config(void);
 
 int main(void) {
-	HAL_Init(); // Reset of all peripherals, init the Flash and Systick
-	SystemClock_Config(); //Configure the system clock
+    HAL_Init(); //Reset of all peripherals, init the Flash and Systick
+  SystemClock_Config(); //Configure the system clock
 
-	/* This example uses HAL library calls to control
-	   the GPIOC peripheral. You’ll be redoing this code
-	   with hardware register access. */
-	___HAL_RCC_GPIOC_CLK_ENABLE(); // Enable the GPIOC clock in the RCC
+  /* This example uses HAL library calls to control
+     the GPIOC peripheral.  You'll be redoing this code
+     with hardware register access. */
 
-	// Set up a configuration struct to pass to the initialization function
-	GPIO_InitTypeDef initStr = {GPIO_PIN_8 | GPIO_PIN_9,
-	                            GPIO_MODE_OUTPUT_PP,
-	                            GPIO_SPEED_FREQ_LOW,
-	                            GPIO_NOPULL};
-	HAL_GPIO_Init(GPIOC, &initStr); // Initialize pins PC8 & PC9
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET); // Start PC8 high
-	while (1) {
-		HAL_Delay(200); // Delay 200ms
-		// Toggle the output state of both PC8 and PC9
-		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9);
-	}
+  __HAL_RCC_GPIOC_CLK_ENABLE(); //Enable the GPIO clock in the RCC
+
+  // Set up a configuration struct to pass to the initialize function
+  GPIO_InitTypeDef initStr = {GPIO_PIN_8 | GPIO_PIN_9,
+                              GPIO_MODE_OUTPUT_PP,
+                              GPIO_SPEED_FREQ_LOW,
+                              GPIO_NOPULL};
+
+  HAL_GPIO_Init(GPIOC, &initStr); // Initializes pins PC8 & PC9
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET); // Start PC* high
+  while (1) {
+    HAL_Delay(200); //Delay 200ms
+    // Toggle the output of both PC8 and PC9
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9);
+  }
 }
+
 // // Original code
 // /**
 //   * @brief  The application entry point.
