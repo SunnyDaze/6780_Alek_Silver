761ec9e79632e06423b1deae9dec33dd30be9d14
SunnyDaze <alekrliddiard@reagan.com>
2026-03-11 18:37:27 -0600

    Implemented code to get the DAC working.  You can now connect a wire from DAC_OUT1 (PA4) to ADC_IN10 (PC0) and it will flash the LEDs back and forth as the voltage goes up and down into the ADC.

--- lab6/Src/lab6.c
+++ lab6/Src/lab6.c
@@ -5,6 +5,22 @@
 
 void SystemClock_Config(void);
 
+// Sine Wave: 8-bit, 32 samples/cycle
+const uint8_t sine_table[32] = {127,151,175,197,216,232,244,251,254,251,244,
+232,216,197,175,151,127,102,78,56,37,21,9,2,0,2,9,21,37,56,78,102};
+
+// Triangle Wave: 8-bit, 32 samples/cycle
+const uint8_t triangle_table[32] = {0,15,31,47,63,79,95,111,127,142,158,174,
+190,206,222,238,254,238,222,206,190,174,158,142,127,111,95,79,63,47,31,15};
+
+// Sawtooth Wave: 8-bit, 32 samples/cycle
+const uint8_t sawtooth_table[32] = {0,7,15,23,31,39,47,55,63,71,79,87,95,103,
+111,119,127,134,142,150,158,166,174,182,190,198,206,214,222,230,238,246};
+
+// Square Wave: 8-bit, 32 samples/cycle
+const uint8_t square_table[32] = {254,254,254,254,254,254,254,254,254,254,
+254,254,254,254,254,254,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
 /**
   * @brief  The application entry point.
   * @retval int
@@ -74,33 +90,71 @@ int main(void)
   uint32_t adcValue;
   uint32_t threshold = 255; // 8-bit max
 
+  /************************************
+  // Lab 6.1
+  // Generating Waveforms with the DAC
+  *************************************/
+
+  // Enable the GPIOA clock in RCC to turn on GPIO Port A
+   RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+
+   // Enable the DAC clock to turn on the DAC
+   RCC->APB1ENR |= RCC_APB1ENR_DACEN;
+
+  // Select PA4 as GPIO pin to use for DAC output
+  // PA4 = DAC_OUT1
+  initStr.Pin = GPIO_PIN_4;
+  initStr.Mode = GPIO_MODE_ANALOG;
+  initStr.Pull = GPIO_NOPULL;
+  My_HAL_GPIO_Init(GPIOA, &initStr);
+
+  // Set the DAC channel to software trigger mode
+  // TSEL = 0x111
+  DAC->CR |= (DAC_CR_TSEL1_0 | DAC_CR_TSEL1_1 |DAC_CR_TSEL1_2);
+
+  // Enable the DAC channel
+  DAC->CR |= DAC_CR_EN1;
+
   while (1)
   {
  
-    adcValue = ADC1->DR;
+    for (int i = 0; i < sizeof(sine_table); i++) {
+    
+      // Turn on LEDs accroding to the ADC's input voltage
+      adcValue = ADC1->DR;
 
-    if (adcValue > (1*(threshold / 4))){
-      My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6,SET);
-    } else {
-      My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6,RESET);
-    }
-    if (adcValue > (2*(threshold / 4))){
-      My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9,SET);
-    } else {
-      My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9,RESET);
-    }
-    if (adcValue > (3*(threshold / 4))){
-      My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7,SET);
-    } else {
-      My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7,RESET);
-    }
-    if (adcValue > (4*(threshold / 4))){
-      My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8,SET);
-    } else {
-      My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8,RESET);
+      if (adcValue > (20)){
+        My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6,SET);
+      } else {
+        My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6,RESET);
+      }
+
+      if (adcValue > (1*(threshold / 3)-20)){
+        My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9,SET);
+      } else {
+        My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9,RESET);
+      }
+
+      if (adcValue > (2*(threshold / 3)-20)){
+        My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7,SET);
+      } else {
+        My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7,RESET);
+      }
+
+      if (adcValue > (255-40)){
+        My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8,SET);
+      } else {
+        My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8,RESET);
+      }
+
+      // Generate a waveform with the DAC converter by sending next value to DAC
+      DAC->DHR8R1 = triangle_table[i];      DAC->SWTRIGR |= DAC_SWTRIGR_SWTRIG1;
+
+      HAL_Delay(40); // Delay 1 ms
     }
 
-    HAL_Delay(100); // Delay 100 ms
+
+
 
   }
   return -1;
